35cb430ff32cd88b3b534eedb55cb4a9a0ec2953
smileywrld <ismaileyyunusa@gmail.com>
2025-07-31 23:59:13 +0100

    updated the JS

--- script.js
+++ script.js
@@ -11,5 +11,5 @@ const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
 
 navToggle.addEventListener("click", () => {
-	navMenu.classList.toggle("hidden");
+	navMenu.classList.toggle("nav-toggle");
 });
