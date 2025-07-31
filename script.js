49263f070de231079eaf1a90b662a5b8c43928ef
smileywrld <ismaileyyunusa@gmail.com>
2025-08-01 00:19:26 +0100

    restarted the mobile menu

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-// 	anchor.addEventListener("click", function (e) {
-// 		e.preventDefault();
-// 		document.querySelector(this.getAttribute("href")).scrollIntoView({
-// 			behavior: "smooth",
-// 		});
-// 	});
-// });
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+	anchor.addEventListener("click", function (e) {
+		e.preventDefault();
+		document.querySelector(this.getAttribute("href")).scrollIntoView({
+			behavior: "smooth",
+		});
+	});
+});
 
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
